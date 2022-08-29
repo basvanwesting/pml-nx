7e3e16873e701b2cfa3cab56147298663a99be5c
basvanwesting <basvanwesting@hotmail.com>
2022-08-29 15:22:21 +0200

    use gradient formula instread of neighbour_models

--- scripts/pizza.exs
+++ scripts/pizza.exs
@@ -1,5 +1,5 @@
 defmodule Pizza do
-  @precision 1.0e-7
+  @precision 1.0e-5
 
   def call do
     x = [13, 2, 14, 23, 13, 1, 18, 10, 26, 3, 3, 21, 7, 22, 2, 27, 6, 10, 18, 15, 9, 26, 8, 15, 10, 21, 5, 6, 13, 13]
@@ -8,27 +8,23 @@ defmodule Pizza do
     tensor = Nx.tensor([x, y], names: [:reservations, :pizzas])
     model = {0.0, 0.0}
     max_iterations = 1000
-    lr = 1.0
+    lr = 1.0e-5
 
     train(tensor, max_iterations, model, lr)
     |> IO.inspect()
   end
 
   def train(_tensor, 0, model, _lr), do: {model, 0}
-  def train(tensor, iterations_left, model, lr) do
+  def train(tensor, iterations_left, {a, b} = model, lr) do
     current_loss = loss(tensor, model)
     IO.inspect({iterations_left, model, current_loss}, label: "iteration, model, loss")
 
-    {next_model, next_loss} =
-      model
-      |> neighbour_models(lr)
-      |> Enum.map(fn neighbour_model -> {neighbour_model, loss(tensor, neighbour_model)} end)
-      |> Enum.min_by(&(elem(&1,1)))
+    gradient = gradient(tensor, model)
+    IO.inspect(gradient, label: "gradient")
 
     cond do
-      next_loss < current_loss -> train(tensor, iterations_left - 1, next_model, lr)
-      iterations_left > 0 && lr > @precision -> train(tensor, iterations_left - 1, model, lr / 2.0)
-      true -> {model, iterations_left}
+      abs(gradient) * lr < @precision -> {model, iterations_left}
+      true -> train(tensor, iterations_left - 1, {a - gradient * lr, b}, lr)
     end
   end
 
@@ -43,14 +39,26 @@ defmodule Pizza do
     |> Nx.to_number()
   end
 
-  def neighbour_models({a, b}, lr) do
-    [
-      {a + lr, b},
-      {a - lr, b},
-      {a, b + lr},
-      {a, b - lr},
-    ]
+  def gradient(tensor, {a, b}) do
+    tensor
+    |> Nx.slice_along_axis(0, 1, axis: :reservations)
+    |> Nx.multiply(a)
+    |> Nx.add(b)
+    |> Nx.subtract(Nx.slice_along_axis(tensor, 1, 1, axis: :reservations))
+    |> Nx.multiply(Nx.slice_along_axis(tensor, 0, 1, axis: :reservations))
+    |> Nx.mean()
+    |> Nx.multiply(2)
+    |> Nx.to_number()
   end
+
+  #def neighbour_models({a, b}, lr) do
+    #[
+      #{a + lr, b},
+      #{a - lr, b},
+      #{a, b + lr},
+      #{a, b - lr},
+    #]
+  #end
 end
 
 Pizza.call()
