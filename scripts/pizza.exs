73530fb3658e105c1105c8019151ed1be01e6298
basvanwesting <basvanwesting@hotmail.com>
2022-08-29 14:54:19 +0200

    ax + b model

--- scripts/pizza.exs
+++ scripts/pizza.exs
@@ -1,6 +1,4 @@
 defmodule Pizza do
-  import Nx.Defn
-
   @precision 1.0e-7
 
   def call do
@@ -8,35 +6,50 @@ defmodule Pizza do
     y = [33, 16, 32, 51, 27, 16, 34, 17, 29, 15, 15, 32, 22, 37, 13, 44, 16, 21, 37, 30, 26, 34, 23, 39, 27, 37, 17, 18, 25, 23]
 
     tensor = Nx.tensor([x, y], names: [:reservations, :pizzas])
-    start_weight = 0.0
+    model = {0.0, 0.0}
     max_iterations = 1000
-    delta = 1.0
+    lr = 1.0
 
-    train(tensor, max_iterations, start_weight, delta)
+    train(tensor, max_iterations, model, lr)
     |> IO.inspect()
   end
 
-  def train(_tensor, 0, weight, _delta), do: {weight, 0}
-  def train(tensor, iterations_left, weight, delta) do
-    current_loss = loss(tensor, weight) |> Nx.to_number #|> IO.inspect(label: "current_loss")
-    delta_up_loss = loss(tensor, weight + delta) |> Nx.to_number #|> IO.inspect(label: "delta_up_loss")
-    delta_down_loss = loss(tensor, weight - delta) |> Nx.to_number #|> IO.inspect(label: "delta_down_loss")
+  def train(_tensor, 0, model, _lr), do: {model, 0}
+  def train(tensor, iterations_left, model, lr) do
+    current_loss = loss(tensor, model)
+    IO.inspect({iterations_left, model, current_loss}, label: "iteration, model, loss")
+
+    {next_model, next_loss} =
+      model
+      |> neighbour_models(lr)
+      |> Enum.map(fn neighbour_model -> {neighbour_model, loss(tensor, neighbour_model)} end)
+      |> Enum.min_by(&(elem(&1,1)))
 
     cond do
-      delta_up_loss < current_loss -> train(tensor, iterations_left - 1, weight + delta, delta)
-      delta_down_loss < current_loss -> train(tensor, iterations_left - 1, weight - delta, delta)
-      iterations_left > 0 && delta > @precision -> train(tensor, iterations_left - 1, weight, delta / 2.0)
-      true -> {weight, iterations_left}
+      next_loss < current_loss -> train(tensor, iterations_left - 1, next_model, lr)
+      iterations_left > 0 && lr > @precision -> train(tensor, iterations_left - 1, model, lr / 2.0)
+      true -> {model, iterations_left}
     end
   end
 
-  defn loss(tensor, weight) do
+  def loss(tensor, {a, b}) do
     tensor
     |> Nx.slice_along_axis(0, 1, axis: :reservations)
-    |> Nx.multiply(weight)
+    |> Nx.multiply(a)
+    |> Nx.add(b)
     |> Nx.subtract(Nx.slice_along_axis(tensor, 1, 1, axis: :reservations))
     |> Nx.power(2)
     |> Nx.mean()
+    |> Nx.to_number()
+  end
+
+  def neighbour_models({a, b}, lr) do
+    [
+      {a + lr, b},
+      {a - lr, b},
+      {a, b + lr},
+      {a, b - lr},
+    ]
   end
 end
 
