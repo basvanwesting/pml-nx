e10ab2e9885d5f0f34049c4620158c915a653f24
basvanwesting <basvanwesting@hotmail.com>
2022-08-29 16:32:44 +0200

    Add fully tensor based multiple weights model

--- scripts/pizza.exs
+++ scripts/pizza.exs
@@ -1,64 +1,57 @@
 defmodule Pizza do
-  @precision 1.0e-5
+  @precision      1.0e-5
+  @lr             1.0e-3
+  @max_iterations 10000
 
   def call do
-    x = [13, 2, 14, 23, 13, 1, 18, 10, 26, 3, 3, 21, 7, 22, 2, 27, 6, 10, 18, 15, 9, 26, 8, 15, 10, 21, 5, 6, 13, 13]
-    y = [33, 16, 32, 51, 27, 16, 34, 17, 29, 15, 15, 32, 22, 37, 13, 44, 16, 21, 37, 30, 26, 34, 23, 39, 27, 37, 17, 18, 25, 23]
+    reservations = [13, 2,  14, 23, 13, 1,  18, 10, 26, 3,  3,  21, 7,  22, 2,  27, 6,  10, 18, 15, 9,  26, 8,  15, 10, 21, 5,  6,  13, 13]
+    temperatures = [26, 14, 20, 25, 24, 12, 23, 18, 24, 14, 12, 27, 17, 21, 12, 26, 15, 21, 18, 26, 20, 25, 21, 22, 20, 21, 12, 14, 19, 20]
+    tourists     = [9,  6,  3,  9,  8,  2,  9,  10, 3,  1,  3,  5,  3,  1,  4,  2,  4,  7,  3,  8,  6,  9,  10, 7,  2,  1,  7,  9,  4,  3]
+    pizzas       = [44, 23, 28, 60, 42, 5,  51, 44, 42, 9,  14, 43, 22, 34, 16, 46, 26, 33, 29, 43, 37, 62, 47, 38, 22, 29, 34, 38, 30, 28]
 
-    tensor = Nx.tensor([x, y], names: [:reservations, :pizzas])
-    model = {0.0, 0.0}
-    max_iterations = 1000
-    lr = 1.0e-5
+    x = Nx.tensor([reservations, temperatures, tourists]) |> Nx.transpose()
+    y = Nx.tensor([pizzas]) |> Nx.transpose()
+    w = Nx.broadcast(0, {3, 1})
 
-    train(tensor, max_iterations, model, lr)
+    train(x, y, w, @max_iterations, @lr)
     |> IO.inspect()
   end
 
-  def train(_tensor, 0, model, _lr), do: {model, 0}
-  def train(tensor, iterations_left, {a, b} = model, lr) do
-    current_loss = loss(tensor, model)
-    IO.inspect({iterations_left, model, current_loss}, label: "iteration, model, loss")
+  def train(_x, _y, w, 0, _lr), do: {w, 0}
+  def train(x, y, w, iterations_left, lr) do
+    #current_loss = loss(x, y, w)
+    #IO.inspect({iterations_left, w, current_loss}, label: "iteration, w, loss")
 
-    gradient = gradient(tensor, model)
-    IO.inspect(gradient, label: "gradient")
+    gradient = gradient(x, y, w)
+    #IO.inspect(gradient, label: "gradient")
 
-    cond do
-      abs(gradient) * lr < @precision -> {model, iterations_left}
-      true -> train(tensor, iterations_left - 1, {a - gradient * lr, b}, lr)
-    end
+    new_w = Nx.subtract(w, Nx.multiply(gradient, lr))
+    train(x, y, new_w, iterations_left - 1, lr)
   end
 
-  def loss(tensor, {a, b}) do
-    tensor
-    |> Nx.slice_along_axis(0, 1, axis: :reservations)
-    |> Nx.multiply(a)
-    |> Nx.add(b)
-    |> Nx.subtract(Nx.slice_along_axis(tensor, 1, 1, axis: :reservations))
+  def predict(x, w) do
+    Nx.dot(x, w)
+  end
+
+  def loss(x, y, w) do
+    x
+    |> Nx.dot(w)
+    |> Nx.subtract(y)
     |> Nx.power(2)
     |> Nx.mean()
-    |> Nx.to_number()
   end
 
-  def gradient(tensor, {a, b}) do
-    tensor
-    |> Nx.slice_along_axis(0, 1, axis: :reservations)
-    |> Nx.multiply(a)
-    |> Nx.add(b)
-    |> Nx.subtract(Nx.slice_along_axis(tensor, 1, 1, axis: :reservations))
-    |> Nx.multiply(Nx.slice_along_axis(tensor, 0, 1, axis: :reservations))
-    |> Nx.mean()
+  def gradient(x, y, w) do
+    diff = x
+    |> Nx.dot(w)
+    |> Nx.subtract(y)
+
+    x
+    |> Nx.transpose()
+    |> Nx.dot(diff)
+    |> Nx.divide(Nx.axis_size(x, 0))
     |> Nx.multiply(2)
-    |> Nx.to_number()
   end
-
-  #def neighbour_models({a, b}, lr) do
-    #[
-      #{a + lr, b},
-      #{a - lr, b},
-      #{a, b + lr},
-      #{a, b - lr},
-    #]
-  #end
 end
 
 Pizza.call()
