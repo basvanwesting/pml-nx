c6459f4181a13dfa164f9d924a371aadd01e28ec
basvanwesting <basvanwesting@hotmail.com>
2022-08-29 16:50:15 +0200

    Add bias to multiple weights model

--- scripts/pizza.exs
+++ scripts/pizza.exs
@@ -1,5 +1,5 @@
 defmodule Pizza do
-  @precision      1.0e-5
+  @precision      1.0e-4
   @lr             1.0e-3
   @max_iterations 10000
 
@@ -7,11 +7,12 @@ defmodule Pizza do
     reservations = [13, 2,  14, 23, 13, 1,  18, 10, 26, 3,  3,  21, 7,  22, 2,  27, 6,  10, 18, 15, 9,  26, 8,  15, 10, 21, 5,  6,  13, 13]
     temperatures = [26, 14, 20, 25, 24, 12, 23, 18, 24, 14, 12, 27, 17, 21, 12, 26, 15, 21, 18, 26, 20, 25, 21, 22, 20, 21, 12, 14, 19, 20]
     tourists     = [9,  6,  3,  9,  8,  2,  9,  10, 3,  1,  3,  5,  3,  1,  4,  2,  4,  7,  3,  8,  6,  9,  10, 7,  2,  1,  7,  9,  4,  3]
+    bias         = Enum.map(reservations, fn _ -> 1 end)
     pizzas       = [44, 23, 28, 60, 42, 5,  51, 44, 42, 9,  14, 43, 22, 34, 16, 46, 26, 33, 29, 43, 37, 62, 47, 38, 22, 29, 34, 38, 30, 28]
 
-    x = Nx.tensor([reservations, temperatures, tourists]) |> Nx.transpose()
+    x = Nx.tensor([bias, reservations, temperatures, tourists]) |> Nx.transpose()
     y = Nx.tensor([pizzas]) |> Nx.transpose()
-    w = Nx.broadcast(0, {3, 1})
+    w = Nx.broadcast(0, {4, 1})
 
     train(x, y, w, @max_iterations, @lr)
     |> IO.inspect()
@@ -19,14 +20,24 @@ defmodule Pizza do
 
   def train(_x, _y, w, 0, _lr), do: {w, 0}
   def train(x, y, w, iterations_left, lr) do
-    #current_loss = loss(x, y, w)
-    #IO.inspect({iterations_left, w, current_loss}, label: "iteration, w, loss")
+    current_loss = loss(x, y, w) |> Nx.to_number()
+    IO.inspect({iterations_left, current_loss}, label: "iteration, loss")
 
     gradient = gradient(x, y, w)
-    #IO.inspect(gradient, label: "gradient")
+    threshold = gradient
+                |> Nx.abs()
+                |> Nx.reduce_max()
+                |> Nx.to_number()
+                |> Kernel.*(lr)
 
-    new_w = Nx.subtract(w, Nx.multiply(gradient, lr))
-    train(x, y, new_w, iterations_left - 1, lr)
+    #IO.inspect({gradient, threshold}, label: "gradient, threshold")
+
+    if threshold < @precision do
+      {w, iterations_left}
+    else
+      new_w = Nx.subtract(w, Nx.multiply(gradient, lr))
+      train(x, y, new_w, iterations_left - 1, lr)
+    end
   end
 
   def predict(x, w) do
